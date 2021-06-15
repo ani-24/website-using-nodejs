99bc676d7a74774d974c6191074c065dc739e2c4
Aniket <rajeshwarkumar9204@gmail.com>
2021-06-15 13:36:09 +0530

    Create folder structure

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const path = require("path");
+const hbs = require("hbs");
 
 // Setting up a port
 const port = process.env.PORT || 3000;
@@ -9,14 +10,16 @@ const port = process.env.PORT || 3000;
 // Setting up paths
 const pubPath = path.join(__dirname, "../public");
 const viewPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // middlewares
 app.use(express.static(pubPath));
 app.set("view engine", "hbs");
 app.set("views", viewPath);
+hbs.registerPartials(partialsPath);
 
 app.get("/", (req, res) => {
-  res.send("Hello, world");
+  res.render("index");
 });
 
 app.listen(port, () => {
