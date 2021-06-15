1a3a0cf9d978e69ec176ab411f52966887bd11f8
Aniket <rajeshwarkumar9204@gmail.com>
2021-06-15 09:55:17 +0530

    create folder structure

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
 // Setting up a port
 const port = process.env.PORT || 3000;
+
+// Setting up paths
+const pubPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, "../templates/views");
+
+// middlewares
+app.use(express.static(pubPath));
+app.set("view engine", "hbs");
+app.set("views", viewPath);
 
 app.get("/", (req, res) => {
   res.send("Hello, world");
